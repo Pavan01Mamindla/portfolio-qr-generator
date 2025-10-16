7874af71e3ae2792133ccefe104e99f546828c3c
Pavan Mamindla <pavanmamindla1@gmail.com>
2025-10-16 21:48:23 +0530

    Delete ecommerce_project directory

--- ecommerce_project/store/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Product
-
-def product_list(request):
-    products = Product.objects.all()
-    context = {
-        'products': products
-    }
-    return render(request, 'store/product_list.html', context)
